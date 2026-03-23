f2ffe267642d8ae015e48168840d076b2c727923
kev-d2007 <kevindbetting@gmail.com>
2026-03-23 14:56:41 +0100

    start files

---
+++ functions.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    function progress_bar_circle() {
+        echo '<div data-preset="circle" class="progress_bar" data-value="35"></div>';
+        echo '<div data-preset="circle" class="progress_bar" data-value="35"></div>';
+        echo '<div data-preset="circle" class="progress_bar" data-value="35"></div>';
+        echo '<div data-preset="circle" class="progress_bar" data-value="35"></div>';
+        echo '<div data-preset="circle" class="progress_bar" data-value="35"></div>';
+        echo '<div data-preset="circle" class="progress_bar" data-value="35"></div>';
+        echo '<div data-preset="circle" class="progress_bar" data-value="35"></div>';
+    }
+    ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coreplanner</title>
+    <link rel="stylesheet" type="text/css" href="loading-bar.css"/>
+    <script type="text/javascript" src="loading-bar.js"></script>
+</head>
+<body>
+    <?php
+    include 'functions.php';
+    $progress_weekly_view = progress_bar_circle();
+    ?>
+</body>
+</html>
