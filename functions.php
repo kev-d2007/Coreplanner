4a0533cb5c2bad5428852470b45d0c8be92ef576
kev-d2007 <kevindbetting@gmail.com>
2026-03-24 10:34:24 +0100

    real-time werkende circle bar

--- functions.php
+++ functions.php
@@ -1,21 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    function progress_bar_circle() {
-        echo '<div data-preset="circle" class="progress_bar" data-value="35"></div>';
-        echo '<div data-preset="circle" class="progress_bar" data-value="35"></div>';
-        echo '<div data-preset="circle" class="progress_bar" data-value="35"></div>';
-        echo '<div data-preset="circle" class="progress_bar" data-value="35"></div>';
-        echo '<div data-preset="circle" class="progress_bar" data-value="35"></div>';
-        echo '<div data-preset="circle" class="progress_bar" data-value="35"></div>';
-        echo '<div data-preset="circle" class="progress_bar" data-value="35"></div>';
+<?php
+function progress_bar_circle($week1 = 0, $week2 = 0, $week3 = 0, $week4 = 0, $week5 = 0, $week6 = 0, $week7 = 0) {
+    $values = [$week1, $week2, $week3, $week4, $week5, $week6, $week7];
+
+    foreach ($values as $index => $value) {
+        $circleIndex = $index + 1;
+        echo '<div data-preset="circle" class="progress_bar" id="circle_' . $circleIndex . '" data-value="' . intval($value) . '"></div>';
     }
-    ?>
-</body>
-</html>
+}
+?>
--- index.php
+++ index.php
@@ -6,11 +6,53 @@
     <title>Coreplanner</title>
     <link rel="stylesheet" type="text/css" href="loading-bar.css"/>
     <script type="text/javascript" src="loading-bar.js"></script>
+
+    <script>
+        // Maak ldBar instanties aan
+        window.onload = function() {
+            for (let i = 1; i <= 7; i++) {
+                window['bar_' + i] = new ldBar("#circle_" + i);
+            }
+        };
+
+        // Update cirkel live
+        function updateCircle(inputElement, weekIndex) {
+            const value = parseInt(inputElement.value) || 0;
+            window['bar_' + weekIndex].set(value);
+        }
+    </script>
 </head>
 <body>
-    <?php
-    include 'functions.php';
-    $progress_weekly_view = progress_bar_circle();
-    ?>
+
+<?php include_once 'functions.php'; ?>
+
+<form>
+    <label>Maandag:</label>
+    <input type="number" onkeyup="updateCircle(this, 1)">
+
+    <label>Dinsdag:</label>
+    <input type="number" onkeyup="updateCircle(this, 2)">
+
+    <label>Woensdag:</label>
+    <input type="number" onkeyup="updateCircle(this, 3)">
+
+    <label>Donderdag:</label>
+    <input type="number" onkeyup="updateCircle(this, 4)">
+
+    <label>Vrijdag:</label>
+    <input type="number" onkeyup="updateCircle(this, 5)">
+
+    <label>Zaterdag:</label>
+    <input type="number" onkeyup="updateCircle(this, 6)">
+
+    <label>Zondag:</label>
+    <input type="number" onkeyup="updateCircle(this, 7)">
+</form>
+
+<?php
+// Genereert automatisch alle 7 cirkels
+progress_bar_circle();
+?>
+
 </body>
 </html>
